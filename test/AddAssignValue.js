070a5d3ae7870d1e11708bd6eba8f53b41dbe25d
Charles Nebo <charles.nebo@hotmail.com>
2022-12-15 12:17:40 +0200

    added test/addAssignValue.js

---
+++ test/AddAssignValue.js
@@ -0,0 +1,31 @@
+import chai, {Assertion, Assertion} from "chai"
+import file from "../src/.internal/addAssignValue.js"
+
+// var chai = require("chai")
+// var file = require ("../src/add")
+// testing branch
+
+const expect = chai.expect
+
+describe("addAssignValueTest", () => {
+
+	it("Assign key value pair, checks if the existing value is not equivalent")
+		function assignValue (object, key, value) {
+		const object = object[key]
+
+        expect (value == value[key])
+		if(!(hasOwnProperty.call(object, key)&&(objectValue, value))){
+			if(value !== 0 || (1 / value) == (1 /objectValue, value))
+				baseAssignValue(object,key, value)
+				baseAssignValue(object, key, value)
+        if (value == undefined && !(key in object)){
+                baseAssignValue(object, key , value)
+		};
+           
+
+	}
+}   
+
+});
+
+	
