f5cebbe5fb49abcca48e94043e00076d375f7a96
Charles Nebo <charles.nebo@hotmail.com>
2022-12-16 18:34:00 +0200

    test/hashTest.js

---
+++ test/baseMergeTest.js
@@ -0,0 +1,47 @@
+import chai from "chai"
+// import file from "../src/.internal/baseMerge.js"
+import { assert } from "chai"
+
+// if using older versions of node
+// var chai = require("chai")
+// var file = require ("../src/add")
+
+const expect = chai.expect
+
+
+describe("baseMergeTest", () => {
+    /*
+    def function customizer (array){
+        TODO: Function to customize merged values
+        returns cuztomized merge
+    }
+    it("Checks if merge source is empty and merge works", () =>{
+        expect(file(object, source, srcIndex, customizer, stack)).to.equal(Merged_object);
+    });
+    it("Checks if object is empty and merge works", () =>{
+        expect(file(object, source, srcIndex, customizer, stack)).to.equal(Merged_object);
+    });
+    it("Checks if object is invalid", () =>{
+        expect(() => file(object, source, srcIndex, customizer, stack))
+        .to.throw(Error), 'object is invalid'    
+    });
+    it("Checks if source is invalid", () =>{
+        expect(() => file(object, source, srcIndex, customizer, stack))
+        .to.throw(Error), 'source is invalid'        
+    });
+    it("Checks if srcIndex is invalid", () =>{
+        expect(() => file(object, source, srcIndex, customizer, stack))
+        .to.throw(Error), 'srcIndex is invalid'    
+    });
+    it("Checks if stack is invalid", () =>{
+        expect(() => file(object, source, srcIndex, customizer, stack))
+        .to.throw(Error), 'stack is invalid'
+    });
+    it("Checks if merge works with simple values", () =>{
+        expect(file(object, source, srcIndex, customizer, stack)).to.equal(Merged_object);
+    });
+    it("Checks if merge works with complicated values", () =>{
+        expect(file(object, source, srcIndex, customizer, stack)).to.equal(Merged_object);
+    });
+    */
+})
---
+++ test/hashTest.js
@@ -0,0 +1,59 @@
+import chai from "chai"
+import file from "../src/.internal/Hash.js"
+import { assert } from "chai"
+
+// if using older versions of node
+// var chai = require("chai")
+// var file = require ("../src/add")
+
+
+const expect = chai.expect
+
+
+describe("hashTest", () => {
+    /*
+    it("Checks if constructor works", () =>{
+        expect(file(entries)).to.equal(hash_object);
+    });
+    it("Creating hash with erroneus entries", () =>{
+         expect(() => file(entries)).to.throw(Error), 'object is faulty'
+    });
+    it("Checks if class hash clear function works", () =>{
+        expect(file(entries).clear).to.equal(cleared_hash);
+    });
+    it("Checks if class hash get function works", () =>{
+        expect(file(entries).get(key)).to.equal(key_entry_value);
+    });
+    it("Checks if class hash get function works with erroneus key", () =>{
+        expect(() => file(entries)).get(key).to.throw(Error), 'key is faulty'
+    });
+    it("Checks if class hash delete function works, key is in hash", () =>{
+        expect(file(entries).delete(key)).to.equal(true);
+    });
+    it("Checks if class hash delete function works, key is not in hash", () =>{
+        expect(file(entries).delete(key)).to.equal(false);
+    });
+    it("Checks if class hash delete function works with erroneus key", () =>{
+        expect(() => file(entries)).delete(key).to.throw(Error), 'key is faulty'
+    });
+    it("Checks if class hash has function works, key is in hash", () =>{
+        expect(file(entries).has(key)).to.equal(true);
+    });
+    it("Checks if class hash has function works, key is not in hash", () =>{
+        expect(file(entries).has(key)).to.equal(false);
+    });
+    it("Checks if class hash has function works with erroneus key", () =>{
+        expect(() => file(entries)).has(key).to.throw(Error), 'key is faulty'
+    });
+    it("Checks if class hash set function works, with correct variables", () =>{
+        expect(file(entries).set(key,value)).to.equal(hash_with_hash_instance);
+    });
+    it("Checks if class hash set function works, with erroneus key", () =>{
+        expect(() => file(entries)).set(key,value).to.throw(Error), 'key is faulty'
+    });
+    it("Checks if class hash set function works, with erroneus value", () =>{
+        expect(() => file(entries)).set(key,value).to.throw(Error), 'value is faulty'
+    });
+    */
+})
+
